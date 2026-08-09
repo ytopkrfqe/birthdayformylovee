7a56d283c67ed85e8fa0320b666320639df481f2
ytopkrfqe <ultimatelysww@gmail.com>
2026-08-10 00:39:17 +0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+# ============================================
+# ===== НАСТРОЙКИ БОТА (ВСЁ ЗАПОЛНИТЬ!) =====
+# ============================================
+
+# 👇 ТОКЕН БОТА ОТ @BotFather
+BOT_TOKEN = "8921736262:AAEPLHpRkebcKhOIMJEhMyQn0B_fPVgpJwE"
+
+# 👇 ССЫЛКА НА ТВОЙ HTML-ФАЙЛ (ГДЕ ОН ЛЕЖИТ)
+# Если залил на GitHub Pages: https://твой-ник.github.io/название/
+# Если на Vercel: https://название.vercel.app
+APP_URL = "https://dazzling-daifuku-c3bd5f.netlify.app"
+
+# 👇 ИМЯ ДЕВУШКИ (такое же как в CONFIG.name в HTML)
+HER_NAME = "Ангелина"
+
+# 👇 ТВОЁ ИМЯ (такое же как в CONFIG.yourName в HTML)
+YOUR_NAME = "Серёжа"
+
+# 👇 ДАТА НАЧАЛА ОТНОШЕНИЙ (ГГГГ-ММ-ДД)
+START_DATE = "2023-01-01"
+
+# 👇 ТВОЙ TELEGRAM USERNAME (для кнопки "спасибо")
+YOUR_USERNAME = "abyssedme1"  # без @
